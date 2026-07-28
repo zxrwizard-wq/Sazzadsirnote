744fd313c486c57c67e61132a37a60206885a029
zxrwizard-wq <zxrwizard@gmail.com>
2026-07-28 15:39:44 +0600

    Create Package.js

---
+++ Package.js
@@ -0,0 +1,17 @@
+{
+  "name": "sazzadsirnote",
+  "version": "1.0.0",
+  "description": "Sazzad Sir Note - category photo gallery with admin upload, MongoDB storage",
+  "main": "server.js",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.19.2",
+    "mongoose": "^8.5.0",
+    "jsonwebtoken": "^9.0.2",
+    "dotenv": "^16.4.5",
+    "cors": "^2.8.5",
+    "multer": "^1.4.5-lts.1"
+  }
+}
